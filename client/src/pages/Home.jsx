d37244f9b3d52cc31039184662716c599ad57496
ninhja <ariannaninh@gmail.com>
2026-03-13 14:33:12 -0700

    removed scaffolding

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,7 @@
 import "../App.css";
 
 function Home() {
-  return (
-    <>
-      <h1>All Food Trucks</h1>
-      <div className="food-trucks"></div>
-    </>
-  );
+  return <></>;
 }
 
 export default Home;
